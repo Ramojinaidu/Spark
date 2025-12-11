d12e6ba14a00ccc519c8deb87c4cb4fedee5f49d
Ramojinaidu <ramoji192358@gmail.com>
2025-12-11 20:52:40 +0530

    changing Spark_app to test

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include<Spark.h>
 int main(){
-    std::cout<<"Spark App";
+    std::cout<<"Spark App\n";
     Spark::Application* app = new Spark::Application();
     app->Run();
     delete app;
