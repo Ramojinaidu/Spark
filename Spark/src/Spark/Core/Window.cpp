4ac667562b9bd9cdd21f6490b8ecd4ab2ef26638
Ramojinaidu <ramoji192358@gmail.com>
2025-12-12 12:20:32 +0530

    changing externalApi to Platform

--- Spark/src/Spark/Core/Window.cpp
+++ Spark/src/Spark/Core/Window.cpp
@@ -1,5 +1,6 @@
 #include "Spark/Core/Window.h"
-#include <glfw_api/glfw_window.h>
+#include <MultiPlatform/glfw_window.h>
+
 namespace Spark {
     std::unique_ptr<Window> Window::Create(const WindowProps& props) {
         return std::make_unique<GlfwWindow>(props);
